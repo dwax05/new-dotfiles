570e6a70e6570f9697e438e51134b8841e4d4925
dwax <djwax12@gmail.com>
2025-10-14 01:55:14 -0400

    made static icon open apple menu & added helper function in c for functionality

--- sketchybar/items/static_icon.sh
+++ sketchybar/items/static_icon.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 sketchybar --add item static_icon left \
     --set static_icon \
     background.color=$SK_COLOR13 \
@@ -7,4 +8,5 @@ sketchybar --add item static_icon left \
     icon="󰅶 " \
     icon.padding_left=7 \
     icon.padding_right=7 \
+    click_script="$PLUGIN_DIR/helpers/bin/menus -s 0" \
     label.drawing=off
